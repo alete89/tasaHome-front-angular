cb8c006a7b8eda0b7ab3fd65326d3816df077707
nicovio <nicolasviotti90@gmail.com>
2019-06-05 17:45:11 -0300

    feat: Agrego mdb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      MDBBootstrapModule.forRoot()
    ],
    providers: [],
    bootstrap: [
