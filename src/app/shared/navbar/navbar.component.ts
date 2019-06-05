ec196f9ba7c18cc9363b5f994c8b7278437f5bab
nicovio <nicolasviotti90@gmail.com>
2019-06-05 17:12:12 -0300

    feat: creo componente navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { UsuarioService } from 'src/app/servicios/usuario.service';
-import { MDBModalRef, MDBModalService } from 'angular-bootstrap-md';
-import { LoginComponent } from '../../login/login.component';
-import { FinalizarCompraComponent } from '../../finalizar-compra/finalizar-compra.component';
 
 @Component({
   selector: 'navbar',
@@ -12,40 +7,9 @@ import { FinalizarCompraComponent } from '../../finalizar-compra/finalizar-compr
 })
 export class NavbarComponent implements OnInit {
 
-  modalRef: MDBModalRef;
-  modalCarrito: MDBModalRef
-
-  constructor(private usuarioService: UsuarioService, private router: Router, private modalService: MDBModalService) { }
+  constructor() { }
 
   ngOnInit() {
   }
-
-  estaLogueado() {
-    return this.usuarioService.estaLogueado()
-  }
-
-  cerrarSesion() {
-    this.usuarioService.cerrarSesion()
-    this.router.navigateByUrl('/home')
-  }
-
-  itemsCarrito() {
-    return this.usuarioService.itemsCarrito()
-  }
-
-  openModal() {
-    this.modalRef = this.modalService.show(LoginComponent, {
-      data: {
-        returnUrl: this.router.url
-      }
-    });
-  }
-
-  // getImagenDePerfil() {
-  //   if (this.estaLogueado()) {
-  //     return this.usuarioService.getImagenDePerfil()
-  //   }
-  // }
-
 
 }
