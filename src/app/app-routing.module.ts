5f577fa94a3ff3cbe4d78b6719fb4e9b70d7253c
nicovio <nicolasviotti90@gmail.com>
2019-06-05 23:38:06 -0300

    nuevos imports y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { TasacionesAnterioresComponent } from './componentes/tasaciones-anteriores/tasaciones-anteriores.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'tasaciones-anteriores', component: TasacionesAnterioresComponent, pathMatch: 'full' },
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './componentes/home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotificationsComponent } from './shared/notifications/notifications.component';
+import { TasacionesAnterioresComponent } from './componentes/tasaciones-anteriores/tasaciones-anteriores.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       HomeComponent,
-      NavbarComponent
+      NavbarComponent,
+      LoginComponent,
+      NotificationsComponent,
+      TasacionesAnterioresComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
+      HttpModule,
+      FormsModule,
+      ReactiveFormsModule,
       MDBBootstrapModule.forRoot()
    ],
    providers: [],
