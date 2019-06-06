b8d5ac62d0809264256967143498a2588a77a4fd
nicovio <nicolasviotti90@gmail.com>
2019-06-05 23:36:07 -0300

    feat: servicio de usuarios

---
+++ src/app/servicios/usuario.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../dominio/usuario';
+import { REST_SERVER_URL } from './configuration';
+import { Http } from '@angular/http';
+import { Tasacion } from '../dominio/tasacion';
+
+export interface IUsuarioService {
+  userLogin(username: string, password: string)
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService implements IUsuarioService {
+
+  constructor(private http: Http) { }
+
+  async userLogin(email: string, password: string) {
+    const url = REST_SERVER_URL + "/login"
+    let json: any = {}
+    json.email = email
+    json.contraseña = password
+    console.log(json)
+    const resp = await this.http.post(url, json).toPromise()
+    const usuario: Usuario = Usuario.fromJson(resp.json())
+    sessionStorage.setItem("userLoggedInId", String(usuario.id));
+    return usuario
+  }
+
+  async tasacionesAnteriores() {
+    const url = REST_SERVER_URL + "/tasaciones_anteriores/" + this.userLoggedInId()
+    const resp = await this.http.get(url).toPromise()
+    return resp.json().map(Tasacion.fromJson)
+  }
+
+  estaLogueado() {
+    return this.userLoggedInId() !== undefined && this.userLoggedInId() !== null
+  }
+
+  userLoggedInId() {
+    return sessionStorage.getItem("userLoggedInId")
+  }
+
+  cerrarSesion() {
+    sessionStorage.clear()
+  }
+
+
+}
+
