01bc3dc0c57a53ddd8fad357263e7dd1014fea9f
nicovio <nicolasviotti90@gmail.com>
2019-06-05 23:36:58 -0300

    fix: cambios en navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MDBModalRef, MDBModalService } from 'angular-bootstrap-md';
+import { Router } from '@angular/router';
+import { LoginComponent } from 'src/app/componentes/login/login.component';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  modalRef: MDBModalRef;
+
+  constructor(private router: Router, private usuarioService: UsuarioService, private modalService: MDBModalService) {
+
+  }
 
   ngOnInit() {
+  }
+
+  estaLogueado() {
+    return this.usuarioService.estaLogueado()
+  }
+
+  cerrarSesion() {
+    this.usuarioService.cerrarSesion()
+    this.router.navigateByUrl('/home')
+  }
+
+  openModal() {
+    this.modalRef = this.modalService.show(LoginComponent, {
+      data: {
+        returnUrl: this.router.url
+      }
+    });
   }
 
 }
