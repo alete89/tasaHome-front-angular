e4602433c3f843fa688f6edeec498ddad0f11f2f
nicovio <nicolasviotti90@gmail.com>
2019-06-17 20:39:21 -0300

    feat: clase tasacion busqueda

---
+++ src/app/dominio/tasacion_busqueda.ts
@@ -0,0 +1,23 @@
+export class TasacionBusqueda {
+
+    ambientes: number
+
+    superficie_minima: number
+
+    fecha_desde: Date
+
+    id_barrio: number
+
+    id_tipo_propiedad: number
+
+    id_tipo_operacion: number
+
+    constructor(init?: Partial<TasacionBusqueda>) {
+        Object.assign(this, init)
+    }
+
+    static fromJson(tasacionJson) {
+        let tasacion = Object.assign(new TasacionBusqueda(), tasacionJson)
+        return tasacion
+    }
+}
