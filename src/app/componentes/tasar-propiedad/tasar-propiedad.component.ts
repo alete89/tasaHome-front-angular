9055f251b308f66a8b77e60dc3a0ef7012cef9bf
nicovio <nicolasviotti90@gmail.com>
2019-06-22 03:09:53 -0300

    feat: tasar propiedad

--- src/app/componentes/tasar-propiedad/tasar-propiedad.component.ts
+++ src/app/componentes/tasar-propiedad/tasar-propiedad.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Tasacion } from 'src/app/dominio/tasacion';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TipoPropiedad } from 'src/app/dominio/tipo_propiedad';
+import { TasacionService } from 'src/app/servicios/tasacion.service';
+import { Estado } from 'src/app/dominio/estado';
+import { EstadoService } from 'src/app/servicios/estado.service';
+import { ServicioService } from 'src/app/servicios/servicio.service';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'app-tasar-propiedad',
@@ -7,9 +15,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasarPropiedadComponent implements OnInit {
 
-  constructor() { }
+  tasacion: Tasacion
+  tiposDePropiedad: Array<TipoPropiedad>
+  tiposDeOperacion: Array<TipoPropiedad>
+  estados: Array<Estado>
+  numbersValidatingForm: FormGroup
 
-  ngOnInit() {
+  constructor(private usuarioService: UsuarioService, private tasacionService: TasacionService, private estadoService: EstadoService, private servicioService: ServicioService) {
+    this.tasacion = new Tasacion()
+    this.numbersValidatingForm = new FormGroup({
+      superficie: new FormControl(null, [Validators.required, Validators.pattern(/^-?[0-9][^\.]*$/), Validators.min(1), Validators.max(100000000)]),
+      ambientes: new FormControl(null, [Validators.required, Validators.pattern(/^-?[0-9][^\.]*$/), Validators.min(1), Validators.max(500)]),
+    })
+  }
+
+  get inputSuperficie() { return this.numbersValidatingForm.get('superficie') }
+  get inputAmbientes() { return this.numbersValidatingForm.get('ambientes') }
+
+
+  superficieInvalida() {
+    return this.inputSuperficie.invalid && (this.inputSuperficie.dirty || this.inputSuperficie.touched)
+  }
+
+  ambientesInvalidos() {
+    return this.inputAmbientes.invalid && (this.inputAmbientes.dirty || this.inputAmbientes.touched)
+  }
+
+  async ngOnInit() {
+    this.tiposDePropiedad = await this.tasacionService.tiposDePropiedad()
+    this.tiposDeOperacion = await this.tasacionService.tiposDeOperacion()
+    this.estados = await this.estadoService.estados()
+    this.tasacion.servicios = await this.servicioService.servicios()
+  }
+
+  async tasar() {
+    this.tasacion.valor = await this.tasacionService.tasarPropiedad(this.tasacion)
+  }
+
+  async guardar() {
+    let respuesta = await this.usuarioService.guardarTasacion(this.tasacion)
+  }
+
+  async publicar() {
+
   }
 
 }
