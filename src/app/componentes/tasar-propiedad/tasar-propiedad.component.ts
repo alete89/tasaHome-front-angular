32dce5e7f5e6155e81ef95aa52357cbef7213c1d
alete89 <alejandrodini@mural.co>
2019-06-27 00:18:33 -0300

    fix: validación de maximos y minimos

--- src/app/componentes/tasar-propiedad/tasar-propiedad.component.ts
+++ src/app/componentes/tasar-propiedad/tasar-propiedad.component.ts
@@ -37,8 +37,8 @@ export class TasarPropiedadComponent implements OnInit {
     this.esActualizacion = false
     this.tasacion = new Tasacion()
     this.numbersValidatingForm = new FormGroup({
-      superficie: new FormControl(null, [Validators.required, Validators.pattern(/^-?[0-9][^\.]*$/), Validators.min(1), Validators.max(100000000)]),
-      ambientes: new FormControl(null, [Validators.required, Validators.pattern(/^-?[0-9][^\.]*$/), Validators.min(1), Validators.max(500)]),
+      superficie: new FormControl(null, [Validators.required, Validators.pattern(/^-?[0-9][^\.]*$/), Validators.min(15), Validators.max(2000)]),
+      ambientes: new FormControl(null, [Validators.required, Validators.pattern(/^-?[0-9][^\.]*$/), Validators.min(1), Validators.max(15)]),
     })
   }
 
